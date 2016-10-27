2253c299315813e0089e6787d63bce5304ecc4a6
Oren Maor <Oren.Maor@citygridmedia.com>
2016-10-27 09:11:36 -0700

    adding SN

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,5 @@
 name             'sed'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'SturdyNetworks'
 license          'All rights reserved'
 description      'Installs/Configures sed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,15 +2,10 @@
 # Cookbook Name:: sed
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2016, Sturdy Networks 
 #
 # All rights reserved - Do Not Redistribute
 #
-
-#execute "blah" do
-#  command "touch /tmp/blah"
-#  action :nothing
-#end
 
 #sed "/tmp/test.properties" do
 #  search "xirrus"
